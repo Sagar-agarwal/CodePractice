97aa47447c4837b117b1004ee6c045db0c964728
Sagar-agarwal <agarwal.sagar@outlook.com>
2017-12-19 12:57:22 +0530

    wdb - 08 JS intro Complete

---
+++ Core/wdb/08/introJS.js
@@ -0,0 +1,10 @@
+// Stalker excercise
+
+var firstName, lastName, age;
+
+firstName = prompt("Enter your First Name");
+lastName = prompt("Enter your Last Name");
+age = prompt("Enter your Age");
+
+console.log("Hi " + firstName + " " + lastName);
+console.log("Are you sure your age is " + age + " ? Doesn't look like to me.");
