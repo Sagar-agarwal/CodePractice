6ab0e5fe836451294374a772b853abe6c00d502e
Sagar-agarwal <agarwal.sagar@outlook.com>
2018-01-21 16:33:51 +0530

    PS: Angular: Get started > M3-Directives__complete

--- learnAngular/pluralSight/getStarted/app.js
+++ learnAngular/pluralSight/getStarted/app.js
@@ -2,14 +2,28 @@ var app = angular.module('app', []);
 
 app.controller('MainController', function ($scope, $http) {
     $scope.message = 'Hello World!!';
+    
 
-    $http.get("https://api.github.com/users/sagar-agarwal")
+    $scope.search = function (username) {
+        $http.get("https://api.github.com/users/" + username)
         .then(function(response){
             $scope.gitUser = response.data;
-        }
-        ,function (reason) {
+            $http.get($scope.gitUser.repos_url)
+                .then(function (response){
+                    $scope.repos = response.data;
+                },
+                function(reason){
+                    $scope.error = "Could not fetch user repos";
+                });
+        },
+        function (reason) {
             $scope.error = "Could not fetch user data";
         });
+    };
+
+
+
+    
 
 });
 
