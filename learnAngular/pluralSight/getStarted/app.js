b5d25683760efadeb4fcb2c643a4cc7181e4d4c2
Sagar-agarwal <agarwal.sagar@outlook.com>
2018-01-23 03:09:38 +0530

    PS: Angular: Get started > M4-Services__complete

--- learnAngular/pluralSight/getStarted/app.js
+++ learnAngular/pluralSight/getStarted/app.js
@@ -1,30 +1,57 @@
 var app = angular.module('app', []);
 
-app.controller('MainController', function ($scope, $http) {
-    $scope.message = 'Hello World!!';
+app.controller('MainController', ["$scope", "github", "$interval", "$timeout", "$log", "$anchorScroll", "$location", 
+    function ($scope, github, $interval, $timeout, $log, $anchorScroll, $location) {
     
+    $scope.message = 'Hello World!!';
+    $scope.username = 'angular';
+    
+    // DIRECTIVES
 
     $scope.search = function (username) {
-        $http.get("https://api.github.com/users/" + username)
-        .then(function(response){
-            $scope.gitUser = response.data;
-            $http.get($scope.gitUser.repos_url)
-                .then(function (response){
-                    $scope.repos = response.data;
-                },
-                function(reason){
-                    $scope.error = "Could not fetch user repos";
-                });
-        },
-        function (reason) {
-            $scope.error = "Could not fetch user data";
-        });
+        $log.info("Searching for" + username);
+        github.getUser(username)
+        .then(
+            function(data){
+                $scope.gitUser = data;
+                github.getRepos($scope.gitUser)
+                    .then(function (data){
+                        $scope.repos = data;
+                        $location.hash("userDetail");
+                        $anchorScroll();
+                        },
+                    function(reason){
+                        $scope.error = "Could not fetch user repos";
+                    });
+            },
+            function (reason) {
+                $scope.error = "Could not fetch user data";
+            },
+        );
+
+        if(countdownInterval){
+            $interval.cancel(countdownInterval);
+        }        
+    };
+
+    // SERVICES
+    $scope.countdown = 5;
+    var decrementCountdown = function (){
+        $scope.countdown -=1;
+
+        if ($scope.countdown < 1){
+            $scope.search($scope.username);
+        }
+    };
+    var countdownInterval = null;
+    function startCountdown (){
+        countdownInterval = $interval(decrementCountdown, 1000, $scope.countdown);
     };
 
 
 
-    
+    startCountdown();
 
-});
+}]);
 
 
