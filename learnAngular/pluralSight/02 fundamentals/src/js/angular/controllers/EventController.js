a54461b91a0f3021860d5cd0b2aff86da842f42c
Sagar-agarwal <agarwal.sagar@outlook.com>
2018-02-14 19:29:17 +0530

    Plural on hold

--- learnAngular/pluralSight/02 fundamentals/src/js/angular/controllers/EventController.js
+++ learnAngular/pluralSight/02 fundamentals/src/js/angular/controllers/EventController.js
@@ -17,13 +17,28 @@ eventsApp.controller('EventController',[ "$scope",
             imageUrl: "src/img/angularjs-logo.png",
             sessions: [
                 {
-                    name: "Directives Masterclass"
+                    name: "Directives Masterclass",
+                    creatorName: "Bob smith",
+                    duration: "1 hr",
+                    level: "Advance",
+                    abstract: "In this session you'll learn in's and out's of Directives",
+                    upVoteCount: 0
                 },
                 {
-                    name: "Scopes for fund & Profits"
+                    name: "Scopes for fund & Profits",
+                    creatorName: "Jay walker",
+                    duration: "3 hr",
+                    level: "Intermidiate",
+                    abstract: "In this session you'll get to know about scope",
+                    upVoteCount: 0
                 },
                 {
-                    name: "Well behaved Controllers"
+                    name: "Well behaved Controllers",
+                    creatorName: "Keeanu Reevs",
+                    duration: "2 hr",
+                    level: "Basic",
+                    abstract: "In this session you'll learn basis of Controllers",
+                    upVoteCount: 0
                 }
             ]
         };
