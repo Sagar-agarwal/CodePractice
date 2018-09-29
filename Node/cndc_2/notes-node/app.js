297d3a64945de210ad71fe0c1cf356d01da185c4
Sagar Agarwal <agarwal.sagar@outlook.com>
2018-09-29 20:39:20 +0530

    cndc2 03 05 complete

--- Node/cndc_2/notes-node/app.js
+++ Node/cndc_2/notes-node/app.js
@@ -1,23 +1,11 @@
 console.log('Starting app.js');
 
+// Node Modules
 const fs = require('fs');
-const os = require('os');
 
+// Third party Modules
 const _ = require('lodash');
+
+// Project Files
 const notes = require('./notes.js');
 
-console.log(_.uniq(['sagar', 'agarwal', 'sagar']));
-
-// console.log(_.isString(true));
-
-// var addResult = notes.add(4, 5);
-// console.log(addResult);
-
-// var res = notes.addNote();
-// console.log(res);
-
-// var user = os.userInfo();
-// fs.appendFile('greeting.txt', '\n Hello ${user.username}!, you are ${notes.age}.', (err) => {
-//     if (err)
-//     console.log('Unable to write to file');
-// });
--- Node/cndc_2/notes-node/notes.js
+++ Node/cndc_2/notes-node/notes.js
@@ -1,12 +1,2 @@
 console.log('starting notes.js');
 
-module.exports.age = 28;
-
-module.exports.addNote = function (){
-    console.log('addNote');
-    return 'add Note';
-};
-
-module.exports.add = function (a, b){
-    return a + b;
-};
