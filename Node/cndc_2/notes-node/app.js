da07deda4c273c782b6358a5efe7c105b371bea6
Sagar Agarwal <agarwal.sagar@outlook.com>
2018-09-29 20:31:06 +0530

    cndc2 03 04 complete

--- Node/cndc_2/notes-node/app.js
+++ Node/cndc_2/notes-node/app.js
@@ -2,17 +2,21 @@ console.log('Starting app.js');
 
 const fs = require('fs');
 const os = require('os');
+
+const _ = require('lodash');
 const notes = require('./notes.js');
 
-var user = os.userInfo();
+console.log(_.uniq(['sagar', 'agarwal', 'sagar']));
 
-var addResult = notes.add(4, 5);
-console.log(addResult);
+// console.log(_.isString(true));
+
+// var addResult = notes.add(4, 5);
+// console.log(addResult);
 
 // var res = notes.addNote();
 // console.log(res);
 
-
+// var user = os.userInfo();
 // fs.appendFile('greeting.txt', '\n Hello ${user.username}!, you are ${notes.age}.', (err) => {
 //     if (err)
 //     console.log('Unable to write to file');
