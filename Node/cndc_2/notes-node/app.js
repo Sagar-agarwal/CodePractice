c6586c3d50bc4537cba57ca02ea14e190bec0d6f
Sagar Agarwal <agarwal.sagar@outlook.com>
2018-09-29 21:46:56 +0530

    cndc2 03 06 complete

--- Node/cndc_2/notes-node/app.js
+++ Node/cndc_2/notes-node/app.js
@@ -9,3 +9,23 @@ const _ = require('lodash');
 // Project Files
 const notes = require('./notes.js');
 
+var command = process.argv[2];
+console.log('Command: ', command);
+
+// add, list, , read, remove,
+switch (command){
+    case 'add':
+        console.log('adding new note');
+        break;
+    case 'list':
+        console.log('fetching existing notes');
+        break;
+    case 'read':
+        console.log('Reading last note');
+        break;
+    case 'remove':
+        console.log('Removing note');
+        break;
+    default:
+        console.log('Operation not recognized in sytem'); 
+}
