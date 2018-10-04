f3459389ed66d60149df5f6e79bb47b848d3a2c0
Sagar Agarwal <agarwal.sagar@outlook.com>
2018-10-04 19:08:02 +0530

    cndc2 03 16 complete

--- Node/cndc_2/notes-node/app.js
+++ Node/cndc_2/notes-node/app.js
@@ -14,16 +14,43 @@ const notes = require('./notes.js');
 
 */
 
-var command = process.argv[2];
+// Reusable Code
+var titleCommandOptions = {
+                            describe: 'Title of note',
+                            demand: true,
+                            alias: 't'
+                            };
+
+var bodyCommandOptions = {
+                            describe: 'Body of note',
+                            demand: 'true',
+                            alias: 'b'
+                            };
+
+
+var argument = process.argv[2];
 //console.log('process: ', command);
 
-var argv = yargs.argv;
+var argv = yargs
+            .command('add', 'Add a new note', {
+                title: titleCommandOptions,
+                body: bodyCommandOptions
+            })
+            .command('remove', 'Remove note with mentioned title', {
+                title: titleCommandOptions
+            })
+            .command('list', 'Display all existing notes', {})
+            .command('read', 'Display note with mentioned title', {
+                title: titleCommandOptions
+            })
+            .help()
+            .argv;
 // console.log('yargs: ', argv);
 
 
 
 // add, list, , read, remove,
-switch (command){
+switch (argument){
     case 'add':
         notes.addNote(argv.title, argv.body);
         break;
