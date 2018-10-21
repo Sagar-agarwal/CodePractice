eb518737244058f3b652cfdec990db8859fb5ee4
Sagar Agarwal <agarwal.sagar@outlook.com>
2018-10-21 23:09:27 +0530

    cndc2 04 06 complete

--- Node/cndc_2/wApp/app.js
+++ Node/cndc_2/wApp/app.js
@@ -1,12 +1,17 @@
 const request = require('request');
 
+// To include your Google API access key, swap this code with your key
 var someObj = require('./../../../someObj.js');
-
 var key = '&key=' + someObj.keys.mapKey + '';
 
 request({
     url: 'https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA' + key,
     json: true
 }, (error, response, body) => {
-    console.log(JSON.stringify(body, undefined, 4));
+    var formattedAddress = body.results[0].formatted_address;
+    var location = body.results[0].geometry.location;
+
+    console.log(`Address: ${formattedAddress}`);
+    console.log('location> Lat: ', location.lat,  ' Lon: ', location.lng);
+    //console.log(JSON.stringify(location, undefined, 4));
 });
