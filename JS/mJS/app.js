b92e2908aa88822e2fb1bff584d10a3c228e46d0
Sagar Agarwal <agarwal.sagar@outlook.com>
2019-02-24 19:24:50 +0530

    js selector methods

---
+++ JS/mJS/app.js
@@ -0,0 +1,16 @@
+// ClassName
+
+let items = document.getElementsByClassName('linkClass');
+
+// items[0].textContent = 'hello link 1';
+// items[1].textContent = 'World link 2';
+
+// Tag name > document.getElementsByTagName
+let tag = document.getElementsByTagName('a');
+
+tag = document.querySelectorAll('a');
+
+
+let val = tag;
+
+console.log(val);
