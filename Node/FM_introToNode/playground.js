e5f519d85c14130ac92da9966f1940ffa24a9a12
Sagar Agarwal <agarwal.sagar@outlook.com>
2019-04-06 13:27:54 +0530

    intro to node FM

---
+++ Node/FM_introToNode/playground.js
@@ -0,0 +1,5 @@
+const fs = require('fs');
+
+// let write = fs.appendFileSync('./hello.js', '\nvar me = "me"');
+// let read = fs.readFileSync('./hello.js', {encoding: 'utf-8'});
+console.log(read);
