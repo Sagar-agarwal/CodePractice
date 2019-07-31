4e3d9cf3b98baf65aa2c6654234cb1aca5249e9c
Sagar Agarwal <agarwal.sagar@outlook.com>
2019-07-31 07:24:05 +0530

    learn angluarjs finish

--- learnAngular/01. LearnAngular/weather/js/app.js
+++ learnAngular/01. LearnAngular/weather/js/app.js
@@ -1,50 +1,54 @@
-var myApp = angular.module('myApp', ['ngRoute', 'ngResource']);
+var myApp = angular.module("myApp", ["ngRoute", "ngResource"]);
 
 // Router
-myApp.config(function ($routeProvider) {
+myApp.config(function($routeProvider) {
     $routeProvider
-        .when('/', {
-            templateUrl: './../angular/views/home.html',
-            controller: 'homeController'
+        .when("/", {
+            templateUrl: "./../angular/views/home.html",
+            controller: "homeController"
         })
-        .when('/forecast', {
-            templateUrl: './../angular/views/forecast.html',
-            controller: 'forecastController'
-        })
-
+        .when("/forecast", {
+            templateUrl: "./../angular/views/forecast.html",
+            controller: "forecastController"
+        });
 });
 
 // SERVICES
 
-myApp.service('cityService', function () {
-    this.city = '';
+myApp.service("cityService", function() {
+    this.city = "";
 });
 
-myApp.service('weatherAPIService', function(){
-    this.api_key = '0822222c66b6501d875d706c13177985';
-})
+myApp.service("weatherAPIService", function() {
+    this.api_key = "6f43030fce3af524b108d19b18cd10cb";
+});
 
 // CONTROLLER
-myApp.controller('homeController', ['$scope', 'cityService',
-    function ($scope, cityService) {
-
+myApp.controller("homeController", [
+    "$scope",
+    "cityService",
+    function($scope, cityService) {
         $scope.city = cityService.city;
 
-        $scope.$watch('city', function () {
+        $scope.$watch("city", function() {
             cityService.city = $scope.city;
         });
     }
 ]);
 
-myApp.controller('forecastController', ['$scope', '$resource', 'cityService', '$log', 'weatherAPIService',
+myApp.controller("forecastController", [
+    "$scope",
+    "$resource",
+    "cityService",
+    "$log",
+    "weatherAPIService",
 
-    function ($scope, $resource, cityService, $log, weatherAPIService) {
-
+    function($scope, $resource, cityService, $log, weatherAPIService) {
         $scope.city = cityService.city;
-        $scope.weatherAPI = $resource('http://api.openweathermap.org/data/2.5/weather');
+        $scope.weatherAPI = $resource("http://api.openweathermap.org/data/2.5/weather");
         $scope.weatherResults = $scope.weatherAPI.get({
             q: $scope.city,
-            APIID: weatherAPIService.api_key
+            apiid: weatherAPIService.api_key
         });
 
         $log.log($scope.weatherResults);
