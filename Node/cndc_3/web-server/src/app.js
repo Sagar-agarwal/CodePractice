34730005029280dd0f9d6762dcd905650e4d111d
Sagar Agarwal <agarwal.sagar@outlook.com>
2020-01-08 10:01:42 +0530

    cnc3 8.3

--- Node/cndc_3/web-server/src/app.js
+++ Node/cndc_3/web-server/src/app.js
@@ -1,42 +1,79 @@
-const hbs = require("hbs");
 const path = require("path");
 const express = require("express");
+const hbs = require("hbs");
+
+const forecast = require("./utils/forecast");
+const geocode = require("./utils/geocode");
 
 const app = express();
 
-hbs.registerPartials(path.join(__dirname, "../views/partials"));
-app.set("view-engin", "hsb");
+// Define paths for Express config
+const publicDirectoryPath = path.join(__dirname, "../public");
+const viewsPath = path.join(__dirname, "../templates/views");
+const partialsPath = path.join(__dirname, "../templates/partials");
 
-hbs.registerHelper("currentYear", () => new Date().getFullYear());
+// Setup handlebars engine and views location
+app.set("view engine", "hbs");
+app.set("views", viewsPath);
+hbs.registerPartials(partialsPath);
 
-app.use(express.static(path.join(__dirname, "../public")));
+// Setup static directory to serve
+app.use(express.static(publicDirectoryPath));
 
 app.get("", (req, res) => {
-    res.render("index.hbs", {
-        title: "Hello there !!"
-    });
-});
-
-app.get("/help", (req, res) => {
-    res.render("help.hbs", {
-        title: "Need any help?"
+    res.render("index", {
+        title: "Weather",
+        name: "Andrew Mead"
     });
 });
 
 app.get("/about", (req, res) => {
-    res.render("about.hbs", {
-        title: "Want to know about us..."
+    res.render("about", {
+        title: "About Me",
+        name: "Andrew Mead"
+    });
+});
+
+app.get("/help", (req, res) => {
+    res.render("help", {
+        helpText: "This is some helpful text.",
+        title: "Help",
+        name: "Andrew Mead"
     });
 });
 
 app.get("/weather", (req, res) => {
-    res.render("weather.hbs", {
-        title: "Its gonna be stormy"
+    if (!req.query.address) {
+        return res.send({
+            error: "You must provide Address"
+        });
+    }
+
+    geocode(req.query.address, (err, answer) => {
+        if (err) {
+            res.send({
+                error: err
+            });
+        } else {
+            res.send(answer);
+        }
+    });
+});
+
+app.get("/help/*", (req, res) => {
+    res.render("404", {
+        title: "404",
+        name: "Andrew Mead",
+        errorMessage: "Help article not found."
     });
 });
 
 app.get("*", (req, res) => {
-    res.render("404.hbs");
+    res.render("404", {
+        title: "404",
+        name: "Andrew Mead",
+        errorMessage: "Page not found."
+    });
 });
 
 app.listen(3000, () => {
