970e7f23d78219cff8c55c2829cd2e8d603dc3bf
Sagar Agarwal <agarwal.sagar@outlook.com>
2020-01-17 09:56:31 +0530

    cndc3 10 complete

--- Node/cndc_3/taskMon/mongodb.js
+++ Node/cndc_3/taskMon/mongodb.js
@@ -18,18 +18,26 @@ mongoClient.connect(
         const db = client.db(databaseName);
         // console.log(db);
 
-        db.collection("users").insertOne(
-            {
-                name: "Sagar",
-                fullName: "Sagar Agarwal"
-            },
-            (error, result) => {
-                if (error) {
-                    return console.log("unable to insert doc");
-                }
-
-                console.log(result.ops);
+        db.collection("users").findOne({ name: "Sag" }, (error, user) => {
+            if (error) {
+                return console.log("Unable to find user");
             }
-        );
+
+            console.log(user);
+        });
+
+        // db.collection("users").insertOne(
+        //     {
+        //         name: "Sagar",
+        //         fullName: "Sagar Agarwal"
+        //     },
+        //     (error, result) => {
+        //         if (error) {
+        //             return console.log("unable to insert doc");
+        //         }
+
+        //         console.log(result.ops);
+        //     }
+        // );
     }
 );
