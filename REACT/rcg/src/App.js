159bbe6dd3ab225e233574ee556b7eb3693dce73
Sagar Agarwal <agarwal.sagar@outlook.com>
2020-02-25 10:07:04 +0530

    rcs complete till 3.18

--- REACT/rcg/src/App.js
+++ REACT/rcg/src/App.js
@@ -3,12 +3,23 @@ import "./App.css";
 import Person from "./Person/Person";
 
 class App extends Component {
+    state = {
+        person: [{ name: "someName", age: 28 }]
+    };
+
+    switchHandler = () => {
+        this.setState({ person: [{ name: "someName", age: 29 }] });
+    };
+
     render() {
         return (
             <div className="App">
                 <h1>Hi, This is React app</h1>
                 <p>Yup, a working REACT app</p>
-                <Person />
+                <button onClick={this.switchHandler}>Switch</button>
+                <Person name={this.state.person[0].name} age={this.state.person[0].age}>
+                    This is a child content
+                </Person>
             </div>
         );
     }
--- REACT/rcg/src/Person/Person.js
+++ REACT/rcg/src/Person/Person.js
@@ -1,7 +1,14 @@
 import React from "react";
 
-const person = () => {
-    return <p>This is a person component</p>;
+const person = props => {
+    return (
+        <div>
+            <p>
+                This is a person component of {props.name} and {props.age}
+            </p>
+            <p>{props.children}</p>
+        </div>
+    );
 };
 
 export default person;
