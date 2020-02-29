9d3be42b981992658f3dc1528a5b5a04f7b671ba
Sagar Agarwal <agarwal.sagar@outlook.com>
2020-02-29 14:19:14 +0530

    rcg 3 complete

--- REACT/rcg/src/App.js
+++ REACT/rcg/src/App.js
@@ -1,19 +1,20 @@
-import React, { useState } from "react";
+import React, { Component } from "react";
 import "./App.css";
 import Person from "./Person/Person";
 
-const App = props => {
-    const [state, setState] = useState({
+class App extends Component {
+    state = {
         person: [
             {
                 name: "someName",
                 age: 28
             }
-        ]
-    });
+        ],
+        other: "some text here"
+    };
 
-    const switchNameHandler = () => {
-        setState({
+    switchNameHandler = () => {
+        this.setState({
             person: [
                 {
                     name: "newName",
@@ -23,13 +24,45 @@ const App = props => {
         });
     };
 
-    return (
-        <div className="App">
-            <h1> Hi, This is React app </h1> <p> Yup, a working REACT app </p>{" "}
-            <button onClick={switchNameHandler}> Switch </button>
-            <Person name={state.person[0].name} age={state.person[0].age} />
-        </div>
-    );
-};
+    nameChangeHandler = event => {
+        this.setState({
+            person: [
+                {
+                    name: event.target.value,
+                    age: 30
+                }
+            ]
+        });
+    };
+
+    render() {
+        const buttonStyle = {
+            backgroundColor: "white",
+            padding: "1rem 4rem",
+            border: "1px solid gray",
+            marginBottom: "10px",
+            boxShadow: "0 2px 3px #eee",
+            fontSize: "1.5rem",
+            fontWeight: "600"
+        };
+
+        return (
+            <div className="App">
+                <h1> Hi, This is React app </h1> <p> Yup, a working REACT app </p>
+                <button style={buttonStyle} onClick={this.switchNameHandler}>
+                    Switch
+                </button>
+                <Person
+                    name={this.state.person[0].name}
+                    age={this.state.person[0].age}
+                    click={this.switchNameHandler}
+                    change={this.nameChangeHandler}
+                >
+                    {this.state.other}
+                </Person>
+            </div>
+        );
+    }
+}
 
 export default App;
--- REACT/rcg/src/Person/Person.js
+++ REACT/rcg/src/Person/Person.js
@@ -1,9 +1,13 @@
 import React from "react";
 
+import "./Person.css";
+
 const person = props => {
     return (
-        <div>
-            <p>
+        <div className="Person">
+            <input type="text" onChange={props.change} />
+
+            <p onClick={props.click}>
                 This is a person component of {props.name} and {props.age}
             </p>
             <p>{props.children}</p>
