098ca1ac7c8b859ba1a7bced40b4f093d1a08a6f
Sagar Agarwal <agarwal.sagar@outlook.com>
2020-03-29 19:24:18 +0530

    cbs initial setup

--- REACT/rcg/src/Components/Persons/Person/Person.js
+++ REACT/rcg/src/Components/Persons/Person/Person.js
@@ -1,17 +1,25 @@
-import React from "react";
+import React, { Component } from "react";
 
 import classes from "./Person.css";
 
-const person = props => {
-    return (
-        <div className={classes.Person}>
-            <input type="text" onChange={props.changed} />
-            <p onClick={props.clicked}>
-                This is a person component of {props.name} and {props.age}
-            </p>
-            <p>{props.children}</p>
-        </div>
-    );
-};
+class Person extends Component {
+    shouldComponentUpdate(nextProps, nextState) {
+        console.log(`[Person.js] shouldComponentUpdate ... 
+            ${this.props.name} != ${nextProps.name}
+        `);
+        return true;
+    }
+    render() {
+        return (
+            <div className={classes.Person}>
+                <input type="text" onChange={this.props.changed} />
+                <p onClick={this.props.clicked}>
+                    This is a person component of {this.props.name} and {this.props.age}
+                </p>
+                <p>{this.props.children}</p>
+            </div>
+        );
+    }
+}
 
-export default person;
+export default Person;
--- REACT/rcg/src/Components/Persons/Persons.js
+++ REACT/rcg/src/Components/Persons/Persons.js
@@ -1,19 +1,28 @@
-import React from "react";
+import React, { Component } from "react";
 import Person from "./Person/Person";
 
-const Persons = props =>
-    props.persons.map((person, index) => {
-        return (
-            <Person
-                name={person.name}
-                age={person.age}
-                clicked={() => props.clicked(index)}
-                key={person.id}
-                changed={event => {
-                    props.changed(event, person.id);
-                }}
-            ></Person>
-        );
-    });
+class Persons extends Component {
+    static getDerivedStateFromProps(props, state) {
+        console.log(`[Persons.js] getDerivedStateFromProps... \n ${state} \n ${props}`);
+        return state;
+    }
+
+    render() {
+        console.log(`[Persons.js] Persons rendering...`);
+        return this.props.persons.map((person, index) => {
+            return (
+                <Person
+                    name={person.name}
+                    age={person.age}
+                    clicked={() => this.props.clicked(index)}
+                    key={person.id}
+                    changed={event => {
+                        this.props.changed(event, person.id);
+                    }}
+                ></Person>
+            );
+        });
+    }
+}
 
 export default Persons;
