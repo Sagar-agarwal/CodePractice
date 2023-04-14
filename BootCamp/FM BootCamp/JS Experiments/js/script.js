d98a0db96629b8b3774fc8dade1d297adc23ccd4
Sagar <agarwal.sagar@outlook.com>
2023-04-14 12:45:27 +0530

    Intro to JS - Scopes done

--- BootCamp/FM BootCamp/JS Experiments/js/script.js
+++ BootCamp/FM BootCamp/JS Experiments/js/script.js
@@ -1,2 +1,22 @@
-let rent = 500;
-console.log(rent);
+/*const multiply = (a, b) => a * b;
+
+const add = (a, b) => a + b;
+
+const subtract = (a, b) => a - b;
+const divide = (a, b) => a / b;
+
+const math = (a, b, func) => func(a, b);
+
+console.log(math(2, 5, subtract));
+*/
+class Matho {
+	constructor(a, b) {
+		this.a = a;
+		this.b = b;
+	}
+
+	add = (a = this.a, b = this.b) => a + b;
+}
+
+const m = new Matho(1, 2);
+console.log(m.add());
