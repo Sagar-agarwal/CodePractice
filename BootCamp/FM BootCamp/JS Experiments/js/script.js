9101e41f5e549248a22b949b3dcfb3294145f916
Sagar <agarwal.sagar@outlook.com>
2023-04-16 02:42:19 +0530

    Intro to JS - Arrays, Objexts and basics of Testing complete

--- BootCamp/FM BootCamp/JS Experiments/js/script.js
+++ BootCamp/FM BootCamp/JS Experiments/js/script.js
@@ -1,22 +1,51 @@
-/*const multiply = (a, b) => a * b;
-
-const add = (a, b) => a + b;
-
-const subtract = (a, b) => a - b;
-const divide = (a, b) => a / b;
-
-const math = (a, b, func) => func(a, b);
-
-console.log(math(2, 5, subtract));
-*/
-class Matho {
-	constructor(a, b) {
-		this.a = a;
-		this.b = b;
-	}
-
-	add = (a = this.a, b = this.b) => a + b;
+function findLargestNumber(list) {
+	// return the largest number in array
+	list.sort((a, b) => a - b).pop();
+	return 0;
 }
 
-const m = new Matho(1, 2);
-console.log(m.add());
+function makeDogObject() {
+	// return a dog object that
+	//   has a speak method that returns woof
+	//   has a name property of "Fido"
+	//   has a color property of "white"
+	//   has an age property of 6
+
+	return {
+		speak: () => "woof",
+		name: "Fido",
+		color: "white",
+		age: 6,
+	};
+}
+
+function getListOfNames(list) {
+	// list is an array of objects that looks like this
+	// { name: "Some Person", jobTitle: "Boss Person", age: 30 }
+	// return a list of all the workers' names
+
+	return list.map((user) => user.name);
+}
+
+function getAverageAge(list) {
+	// using the same list as above, get the average age of all the workers'
+	let age = 0;
+	list.forEach((user) => (age += user.age));
+
+	return age / list.length;
+}
+
+function getAllEngineers(list) {
+	// using the same list as above, return a list of all people who have the word "Engineer" in their jobTitle
+
+	return list.filter(({ jobTitle }) => jobTitle.toLowerCase().indexOf("engineer") >= 0).map(({ name }) => name);
+	// return ["Altaïr", "Ezio", "Connor", "Bayek", "Alexios"];
+}
+
+function sort(list) {
+	// sort a list of numbers into ascending order
+	// if you need more help, Google how to do bubble sort
+	// you can sort, if you want to write it by hand, use bubble sort or insertion sort
+
+	return list.sort((a, b) => a - b);
+}
