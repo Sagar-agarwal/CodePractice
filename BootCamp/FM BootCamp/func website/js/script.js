36822e5fa3d349a0a6e69685b57b1e2e761f741c
Sagar <agarwal.sagar@outlook.com>
2023-04-20 19:47:03 +0530

    Website - Cat - Carousel

--- BootCamp/FM BootCamp/func website/js/script.js
+++ BootCamp/FM BootCamp/func website/js/script.js
@@ -1,1 +1,60 @@
-console.log(catKey);
+let data = "";
+let imgURLs = [];
+let index = 0;
+
+const next = document.querySelector(".next").addEventListener("click", (e) => {
+	// debugger;
+	if (index < 10) {
+		index++;
+		updateImageSrc();
+	} else {
+		return;
+	}
+	e.preventDefault();
+});
+const back = document.querySelector(".back").addEventListener("click", (e) => {
+	if (index > 0) {
+		index--;
+		updateImageSrc();
+	} else {
+		return;
+	}
+	e.preventDefault();
+});
+
+const header = new Headers();
+header.append("Access-Control-Allow-Origin", "http://127.0.0.1:5500");
+
+const getCats = () => {
+	return fetch("https://api.thecatapi.com/v1/images/search?limit=10&breed_ids=beng&api_key=REPLACE_ME", {
+		header: header,
+	})
+		.then((res) => res.json())
+		.then((resData) => {
+			init(resData);
+			data = resData;
+			return data;
+		})
+		.catch((err) => err);
+};
+
+const parseDataForImageURLS = (data) => {
+	imgURLs = data.map((ele) => ele.url);
+	updateImageSrc(imgURLs[index]);
+	return;
+};
+
+const init = (data) => {
+	parseDataForImageURLS(data);
+};
+
+const updateImageSrc = () => {
+	const img = document.querySelector("#image");
+	img.src = imgURLs[index];
+};
+
+(() => {
+	// getCats();
+
+	init(sampleInput);
+})();
