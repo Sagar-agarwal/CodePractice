0e69040356c474a1616cc4ff13ca22a5e0ed821c
Sagar <agarwal.sagar@outlook.com>
2023-04-20 13:47:51 +0530

    Added JS Experiements files

--- BootCamp/FM BootCamp/JS Experiments/js/script.js
+++ BootCamp/FM BootCamp/JS Experiments/js/script.js
@@ -1,3 +1,15 @@
+let redSquare = document.querySelector(".red-square");
+redSquare.addEventListener("click", (e) => {
+	redSquare.style.backgroundColor = getRandomHexColorCode();
+	console.log(e);
+});
+
+const getRandomHexColorCode = () => {
+	let n = (Math.random() * 0xfffff * 1000000).toString(16);
+	return "#" + n.slice(0, 6);
+};
+
+// ----------------------------------------------------------------//
 function findLargestNumber(list) {
 	// return the largest number in array
 	list.sort((a, b) => a - b).pop();
